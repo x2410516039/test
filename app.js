96a2150a29070fe82de43542db47f68ce8ef339f
x2410516039 <2410516039@qq.com>
2026-05-14 20:39:14 +0800

    git-test 1.0.1 看看分支长啥样

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const state = {
   tasks: [
     { id: 1, title: "查看 README，了解项目结构", done: true },
-    { id: 2, title: "创建第一个功能分支", done: false },
+    { id: 2, title: "创建第一个功能分支", done: true },
     { id: 3, title: "合并分支回 main", done: false },
   ],
 };
